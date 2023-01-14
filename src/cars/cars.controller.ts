dae953589244ac87781a1346e63a80cd279308d4
Sergio Quintana <sergioq15@hotmail.com>
2023-01-14 11:26:33 -0500

    create the first basic crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch, Delete, ParseUUIDPipe, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 @Controller('cars')
+/* @UsePipes(ValidationPipe) */
 export class CarsController {
+
+  constructor(
+    private readonly carsService: CarsService
+  ) { }
 
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.carsService.findAll();
   }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto) {
+    return this.carsService.update(id, updateCarDto);
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id);
+  }
+
+
 
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,89 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid'
+import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
+
+
+@Injectable()
+export class CarsService {
+
+    private cars: Car[] = [
+        {
+            id: uuid(),
+            brand: 'Toyota',
+            model: 'Corolla',
+        },
+        {
+            id: uuid(),
+            brand: 'Honda',
+            model: 'Civic',
+        },
+        {
+            id: uuid(),
+            brand: 'Jeep',
+            model: 'Cherokee',
+        },
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: string) {
+        const car = this.cars.find(car => car.id === id);
+
+        if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
+        return car;
+    }
+
+    create(createCarDto: CreateCarDto) {
+        const newCar = {
+            id: uuid(),
+            ...createCarDto
+            /* brand: createCarDto.brand,
+            model: createCarDto.model, */
+        }
+        this.cars.push(newCar);
+        return newCar;
+    }
+
+    update(id: string, updateCarDto: UpdateCarDto) {
+
+        let carDB = this.findOneById(id);
+
+        if (updateCarDto.id && updateCarDto.id !== id)
+            throw new BadRequestException(`Car with id '${id}' is not valid inside body`);
+
+
+        this.cars = this.cars.map(car => {
+
+            if (car.id === id) {
+                carDB = {
+                    ...carDB,
+                    ...updateCarDto,
+                    id,
+                }
+                return carDB;
+            }
+            return car;
+        });
+
+        return carDB;
+    }
+
+    delete(id: string) {
+        this.findOneById(id);
+        /* if (carDB.id !== id)
+            throw new BadRequestException(`Car with id '${id}' no exist`); */
+
+        this.cars = this.cars.filter((car) => car.id !== id);
+
+        return {
+            message: 'Car eliminated'
+        }
+
+    }
+
+}
